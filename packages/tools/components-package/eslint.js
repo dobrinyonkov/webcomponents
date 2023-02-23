46f97b3f00eeef4a4eca5169387969e7171195b1
Dobrin Dimchev <dobrin.dimchev@sap.com>
2023-02-23 16:59:47 +0200

    feat(playground): add eslint-mdx

--- packages/tools/components-package/eslint.js
+++ packages/tools/components-package/eslint.js
@@ -10,6 +10,7 @@ module.exports = {
 		parser: "@typescript-eslint/parser",
 		plugins: ["@typescript-eslint"],
 		extends: [
+			"plugin:mdx/recommended",
 			"plugin:@typescript-eslint/recommended",
 			"plugin:@typescript-eslint/recommended-requiring-type-checking"
 		],
