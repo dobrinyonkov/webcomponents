b0f2d1b2b0ca3027300b1c630995d5d8ae4bd5e7
Dobrin Dimchev <dobrin.dimchev@sap.com>
2023-02-23 17:04:41 +0200

    fix: overrides

--- packages/tools/components-package/eslint.js
+++ packages/tools/components-package/eslint.js
@@ -6,9 +6,9 @@ module.exports = {
 	"root": true,
 	"extends": "airbnb-base",
 	overrides: [{
-		files: '*.md',
+		files: '^(?!README\.md$).*\.md$',
 		extends: 'plugin:mdx/recommended',
-	  }, {
+	}, {
 		files: ["*.ts"],
 		parser: "@typescript-eslint/parser",
 		plugins: ["@typescript-eslint"],
