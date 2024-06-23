c77e4570e26cfe574fceeb9b2d7d319a02b018a0
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2024-06-23 00:12:56 +0000

    Deploying to gh-pages from @ dobrinyonkov/ui5-webcomponents@8f5b67901a429196252f52980c10a04f7d43fe0b 🚀

--- nightly/assets/test/pages/ComponentStyles.html-2d0e6669.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-591dad80.js";const n="_inputIcon_1ojd7_2";const o=document.getElementById("custom-icon");o.classList.add(n);
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.5",
     isNext: false,
-    buildTime: 1719014910,
+    buildTime: 1719101342,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
