3661789b6b4bb2d41815d1350a33d2d295b98416
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2024-08-08 00:12:59 +0000

    Deploying to gh-pages from @ dobrinyonkov/ui5-webcomponents@b337d98357fad311b8c4d0eb04fcefe69f81e128 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.6",
     isNext: false,
-    buildTime: 1722989330,
+    buildTime: 1723075737,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
--- nightly/local-cdn/main/dist/assets/test/pages/ComponentStyles.html-367108c8.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-69bb398e.js";const n="_inputIcon_1wpmw_2";const o=document.getElementById("custom-icon");o.classList.add(n);
