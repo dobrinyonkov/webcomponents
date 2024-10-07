13b1f2b5ff29891ea658bafafdfe8368c7736de4
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2024-10-07 00:14:12 +0000

    Deploying to gh-pages from @ dobrinyonkov/ui5-webcomponents@b337d98357fad311b8c4d0eb04fcefe69f81e128 🚀

--- nightly/assets/test/pages/Icon.html-d8916bd7.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-d3ecb3c5.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
