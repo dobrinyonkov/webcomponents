f619ee2da15429408e16547bea3df4d042a68eb7
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2025-03-09 00:13:06 +0000

    Deploying to gh-pages from @ dobrinyonkov/ui5-webcomponents@60fca15cac836a74671a8d1b9fedcca006c71d8d 🚀

--- nightly/local-cdn/base/dist/prod/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/prod/generated/VersionInfo.js
@@ -1,2 +1,2 @@
-"use strict";const e={version:"2.8.0-rc.0",major:2,minor:8,patch:0,suffix:"-rc.0",isNext:!1,buildTime:1741392520};export default e;
+"use strict";const e={version:"2.8.0-rc.0",major:2,minor:8,patch:0,suffix:"-rc.0",isNext:!1,buildTime:1741478959};export default e;
 //# sourceMappingURL=VersionInfo.js.map
