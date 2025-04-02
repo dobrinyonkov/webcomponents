1fb5a407fe7dd3840ebe698f97b00666fcd113fc
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2025-04-02 00:14:09 +0000

    Deploying to gh-pages from @ dobrinyonkov/ui5-webcomponents@60fca15cac836a74671a8d1b9fedcca006c71d8d 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.0",
     isNext: false,
-    buildTime: 1743466323,
+    buildTime: 1743552623,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
