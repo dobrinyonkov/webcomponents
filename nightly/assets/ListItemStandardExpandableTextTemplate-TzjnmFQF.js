470361ae2f36ad7ca9c46f08426f902d4626618c
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2025-07-08 00:15:55 +0000

    Deploying to gh-pages from @ dobrinyonkov/ui5-webcomponents@035fa036870048fe927b8be19bcb15556708f4d2 🚀

--- nightly/assets/ListItemStandardExpandableTextTemplate-TzjnmFQF.js
+++
@@ -1,1 +0,0 @@
-import{a as x,E as m}from"./bundle.esm-DI-brtyQ.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.2",
     isNext: false,
-    buildTime: 1751847169,
+    buildTime: 1751933522,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
