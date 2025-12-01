b9e3eb57e629cfce2985c469736edef5328a6dce
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2025-12-01 00:18:23 +0000

    Deploying to gh-pages from @ dobrinyonkov/webcomponents@6312c9df02331b7fa3df95838af5f1dcb36c2345 🚀

--- nightly/local-cdn/main/dist/assets/ListItemStandardExpandableTextTemplate-DTt4JdxS.js
+++
@@ -1,1 +0,0 @@
-import{a as x,E as m}from"./bundle.esm-DPZ_14Xn.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
