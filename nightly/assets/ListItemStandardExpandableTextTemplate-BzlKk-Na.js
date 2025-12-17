9ffcd816e9c3fce94fa045674132a7b6f74dad55
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2025-12-17 00:15:32 +0000

    Deploying to gh-pages from @ dobrinyonkov/webcomponents@6312c9df02331b7fa3df95838af5f1dcb36c2345 🚀

--- nightly/assets/ListItemStandardExpandableTextTemplate-BzlKk-Na.js
+++
@@ -1,1 +0,0 @@
-import{a as x,E as m}from"./bundle.esm-D9H9Q08Y.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.1",
     isNext: false,
-    buildTime: 1765843910,
+    buildTime: 1765930251,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
