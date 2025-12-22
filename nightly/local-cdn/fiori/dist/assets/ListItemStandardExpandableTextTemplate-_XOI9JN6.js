ac4bdf37beebda6645dabb3492037ea0ccb75eb1
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2025-12-22 00:16:56 +0000

    Deploying to gh-pages from @ dobrinyonkov/webcomponents@6312c9df02331b7fa3df95838af5f1dcb36c2345 🚀

--- nightly/local-cdn/fiori/dist/assets/ListItemStandardExpandableTextTemplate-_XOI9JN6.js
+++
@@ -1,1 +0,0 @@
-import{ae as x,av as m}from"./bundle.esm-CpwJp58r.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
