2d473cee2d43ec045fa8de316c230202151f7731
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2026-01-29 00:18:34 +0000

    Deploying to gh-pages from @ dobrinyonkov/webcomponents@6312c9df02331b7fa3df95838af5f1dcb36c2345 🚀

---
+++ nightly/assets/test/pages/Icon.html-CZvU7DF1.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-Lh9eYEJy.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.1",
     isNext: false,
-    buildTime: 1769559170,
+    buildTime: 1769645648,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
