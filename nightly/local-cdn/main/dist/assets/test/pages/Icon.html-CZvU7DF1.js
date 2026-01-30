163ab774c8caea8d8118ae09d960b9149170dae7
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2026-01-30 00:18:58 +0000

    Deploying to gh-pages from @ dobrinyonkov/webcomponents@6312c9df02331b7fa3df95838af5f1dcb36c2345 🚀

--- nightly/local-cdn/main/dist/assets/test/pages/Icon.html-CZvU7DF1.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-Lh9eYEJy.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
---
+++ nightly/local-cdn/main/dist/assets/test/pages/Icon.html-UMgf-lfM.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-CJ_QZ99_.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
