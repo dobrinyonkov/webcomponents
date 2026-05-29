801112c55f345863ff2c011b1a3656d01555508f
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2026-05-29 00:41:11 +0000

    Deploying to gh-pages from @ dobrinyonkov/webcomponents@6312c9df02331b7fa3df95838af5f1dcb36c2345 🚀

--- nightly/local-cdn/base/dist/prod/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/prod/generated/VersionInfo.js
@@ -1,2 +1,2 @@
-"use strict";const e={version:"2.16.0-rc.1",major:2,minor:16,patch:0,suffix:"-rc.1",isNext:!1,buildTime:1779928301};export default e;
+"use strict";const e={version:"2.16.0-rc.1",major:2,minor:16,patch:0,suffix:"-rc.1",isNext:!1,buildTime:1780015e3};export default e;
 //# sourceMappingURL=VersionInfo.js.map
