66d402b3d4e00bf0d73c1159c82af452a54439d6
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2026-07-03 02:14:19 +0000

    Deploying to gh-pages from @ dobrinyonkov/webcomponents@6312c9df02331b7fa3df95838af5f1dcb36c2345 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.1",
     isNext: false,
-    buildTime: 1782952556,
+    buildTime: 1783044595,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
